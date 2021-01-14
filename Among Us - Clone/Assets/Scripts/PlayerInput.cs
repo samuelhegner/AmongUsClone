d957611407c89dc19de3e505f48493bf9dadf17c
Samuel Hegner <samuel.hegner@gmail.com>
2021-01-14 15:09:51 +0000

    Player WASD Input Even Set up

--- Among Us - Clone/Assets/Scripts/PlayerInput.cs
+++ Among Us - Clone/Assets/Scripts/PlayerInput.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerInput : MonoBehaviour
 {
+    [SerializeField] private InputAction verticalHorizontalInput;
+
+    private Vector2 verticalHorizontalInputValue;
+
+    public Vector2 VerticalHorizontalInputValue { get => verticalHorizontalInputValue; set => verticalHorizontalInputValue = value; }
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        verticalHorizontalInput.performed += ctx => verticalHorizontalInputValue = ctx.ReadValue<Vector2>();
+        verticalHorizontalInput.canceled += ctx => verticalHorizontalInputValue = Vector2.zero;
+    }
+ 
+
+    private void OnEnable()
+    {
+        verticalHorizontalInput.Enable();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        verticalHorizontalInput.Disable();
     }
 }
